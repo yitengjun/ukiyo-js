28bc3cc3498159554ffed2a44fed9fc491b6da56
yitengjun <ito.yiteng.jun@gmail.com>
2023-03-15 14:55:32 +0900

    fix: change output format

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import pluginTypescript from '@rollup/plugin-typescript';
 import buble from '@rollup/plugin-buble';
 import terser from '@rollup/plugin-terser';
+import pkg from './package.json';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -16,10 +17,15 @@ export default {
   input: './src/ukiyo.ts',
   output: [
     {
-      sourcemap: false,
-      file: './dist/ukiyo.min.js',
-      format: 'umd',
       name: 'Ukiyo',
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: false,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: false,
     },
     {
       file: './docs/js/ukiyo.min.js',
