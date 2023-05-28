c7e53d405f6ed435fa9098fcaa80bba432589d10
yitengjun <ito.yiteng.jun@gmail.com>
2023-05-28 18:16:13 +0900

    docs: update file and directory cleanup

--- docs/js/dist/types/options.d.ts
+++
@@ -1,9 +0,0 @@
-type Defaults = {
-    readonly scale: number;
-    readonly speed: number;
-    readonly wrapperClass: string | null;
-    readonly willChange: boolean;
-    readonly externalRAF: boolean;
-};
-export declare const defaults: Defaults;
-export {};
